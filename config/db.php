73d820e071a972fd77f8bbf8425d7f9f3f620d96
Oleg <olegpv@yandex.ru>
2016-10-12 14:27:28 +0300

    user

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2news',
+    'username' => 'yii2news',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
